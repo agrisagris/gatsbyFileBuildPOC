cb72fd156c4a9e7c80a05188e7c66ca74f67d372
agris <agris.melko@gmail.com>
2021-05-03 13:54:25 +0300

    Added file listing

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,25 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Node.js (TypeScript) API',
+    title: "Gatsby + Node.js (TypeScript) API",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Gatsby + Node.js (TypeScript) API',
-        short_name: 'Gatsby + Node.js (TypeScript)',
-        start_url: '/',
-        icon: 'src/images/gatsby-icon.png',
+        name: "Gatsby + Node.js (TypeScript) API",
+        short_name: "Gatsby + Node.js (TypeScript)",
+        start_url: "/",
+        icon: "src/images/gatsby-icon.png",
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    `gatsby-plugin-mdx`,
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet';
-import '../styles/index.css';
+import { graphql } from "gatsby";
+import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+import "../styles/index.css";
 
-function Index() {
+function Index({ data }) {
   const [date, setDate] = useState(null);
   useEffect(() => {
     async function getDate() {
-      const res = await fetch('/api/date');
+      const res = await fetch("/api/date");
       const newDate = await res.text();
       setDate(newDate);
     }
@@ -15,42 +16,42 @@ function Index() {
   return (
     <main>
       <Helmet>
-        <title>Gatsby + Node.js (TypeScript) API</title>
+        <title>Gatsby file build POC</title>
       </Helmet>
       <h1>Gatsby + Node.js (TypeScript) API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/main/examples/gatsby"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        is a <a href="https://www.gatsbyjs.org/">Gatsby</a> app with two
-        directories, <code>/src</code> for static content and <code>/api</code>{' '}
-        which contains a serverless{' '}
-        <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Node.js (TypeScript)
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Node.js (TypeScript) is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      {data?.allMdx?.nodes.map((file) => {
+        return (
+          <div key={file?.id}>
+            <h2>{file?.slug}</h2>
+
+            <h3>{file?.frontmatter?.title}</h3>
+            <p>{file?.frontmatter?.url1}</p>
+            <p>{file?.frontmatter?.url2}</p>
+          </div>
+        );
+      })}
     </main>
   );
 }
 
 export default Index;
+
+export const pageQuery = graphql`
+  {
+    allMdx {
+      nodes {
+        id
+        slug
+        frontmatter {
+          title
+          url1
+          url2
+        }
+        headings {
+          value
+          depth
+        }
+      }
+    }
+  }
+`;
